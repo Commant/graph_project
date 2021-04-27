d62170708d88863f6eef7ad42a9e822aeb160895
BAUCHER Edgar <Edgar.Baucher@bordeaux-inp.fr>
2021-04-27 11:56:55 +0200

    debut

---
+++ graph.h
@@ -0,0 +1,26 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#define NO_LINK (-1.0)
+
+struct graph
+{
+	int n;//nombre de sommet = 1+(dechets+obstacles*4)^2
+	float** M;//Matrice telle que M[i][j] = distance entre i et j
+	//                                    = NO_LINK s'il y un obstacle entre i et j	
+};
+
+struct graph* graph__create(int n_vertice);
+
+void graph__fill(struct graph* g, float distance);
+
+void graph__set_distance(struct graph* g, int i, int j, float distance);
+
+float graph__get_distance(struct graph* g, int i, int j);
+
+//retourne 0 s'il y a un obstacle entre i et j, et 1 s'il sont liés
+int graph__is_linked(struct graph* g, int i, int j);
+
+void graph__free(struct graph* g);
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+//sommet: i:j = sommet du point i sachant qu'on vient du point j 
+//poids(i:k --> j:i)=d(i-j)/v + angle(k-i-j)/va
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
