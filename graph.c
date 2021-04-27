a4f7b6197f498168ed4dce779964bd5d206ae5c1
BAUCHER Edgar <Edgar.Baucher@bordeaux-inp.fr>
2021-04-27 12:11:33 +0200

    oup

--- graph.c
+++ graph.c
@@ -2,15 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct graph* graph__create(int n_vertice);
+struct graph* graph__create(int n_vertice)
+{
+	return NULL;
+}
 
-void graph__fill(struct graph* g, float distance);
+void graph__fill(struct graph* g, float distance)
+{
 
-void graph__set_distance(struct graph* g, int i, int j, float distance);
+}
 
-float graph__get_distance(struct graph* g, int i, int j);
+void graph__set_weight(struct graph* g, int i, int j, float distance)
+{
 
+}
+
+float graph__get_weight(struct graph* g, int i, int j)
+{
+	return 0.0;
+}
 //retourne 0 s'il y a un obstacle entre i et j, et 1 s'il sont liés
-int graph__is_linked(struct graph* g, int i, int j);
+int graph__is_linked(struct graph* g, int i, int j)
+{
+	return 0;
+}
 
-void graph__free(struct graph* g);
+void graph__free(struct graph* g)
+{
+
+}
--- graph.h
+++ graph.h
@@ -14,9 +14,9 @@ struct graph* graph__create(int n_vertice);
 
 void graph__fill(struct graph* g, float distance);
 
-void graph__set_distance(struct graph* g, int i, int j, float distance);
+void graph__set_weight(struct graph* g, int i, int j, float distance);
 
-float graph__get_distance(struct graph* g, int i, int j);
+float graph__get_weight(struct graph* g, int i, int j);
 
 //retourne 0 s'il y a un obstacle entre i et j, et 1 s'il sont liés
 int graph__is_linked(struct graph* g, int i, int j);
