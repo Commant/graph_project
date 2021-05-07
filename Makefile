8c3e4ad77728d5b2712261c814489bb29544ba95
BAUCHER Edgar <Edgar.Baucher@bordeaux-inp.fr>
2021-05-07 10:05:52 +0200

    test 1

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ WR=-Wall
 
 all:code
 
-code:graph.o build_graph.o path.o main.c
-	${COM} ${WR} -o robot graph.o build_graph.o path.o main.c -lm
+code:graph.o build_graph.o path.o create_graph.o main.c
+	${COM} ${WR} -o robot graph.o create_graph.o build_graph.o path.o main.c -lm
 
 graph.o: graph.c
 	${COM} ${WR} -c graph.c
@@ -14,6 +14,9 @@ build_graph.o: build_graph.c
 
 path.o: path.c
 	${COM} ${WR} -c path.c
+
+create_graph.o: create_graph.c
+	${COM} ${WR} -c create_graph.c
 
 clean:
 	rm *.o
--- main.c
+++ main.c
@@ -3,26 +3,18 @@
 #include "graph.h"
 #include "build_graph.h"
 #include "path.h"
+#include "create_graph.h"
 
 //sommet: i:j = sommet du point i sachant qu'on vient du point j 
 //poids(i:k --> j:i)=d(i-j)/v + angle(k-i-j)/va
 
 int main(int argc, char* argv[])
 {
-	struct data* d=load_data("./coords.txt");
+	struct data* d=load_data("./test.txt");
 	print_data(d);
 	free_data(d);
 
-	struct graph* g=graph__create(15);
-	graph__fill(g,0.0);
-	graph__untie(g,6,5);
-	graph__untie(g,3,11);
-	graph__untie(g,14,13);
-	graph__set_weight(g,8,5,15.0);
-	graph__set_weight(g,1,5,3.0);
-	graph__set_weight(g,2,14,7.0);
-	graph__fill_vertice(g,4,2.0);
-	graph__fill_diagonal(g,0.0);
+	struct graph* g=build_graph(d,10.0,5.0);
 
 	graph__print(g);
 	graph__free(g);
